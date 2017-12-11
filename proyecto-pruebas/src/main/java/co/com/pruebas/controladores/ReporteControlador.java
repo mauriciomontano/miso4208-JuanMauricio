d6bfa3f61eef3b1edd09a94a89db7cda17050735
juan <juancamilogaviriaacosta@gmail.com>
2017-12-10 19:47:29 -0500

    estable

--- proyecto-pruebas/src/main/java/co/com/pruebas/controladores/ReporteControlador.java
+++ proyecto-pruebas/src/main/java/co/com/pruebas/controladores/ReporteControlador.java
@@ -34,14 +34,37 @@ public class ReporteControlador implements Serializable {
 
     private void generarReporte() {
         try {
-
             barModelMonkey = new BarChartModel();
-
+            
+            URL r1 = this.getClass().getResource("/monkey.log");
+            File a1 = new File(r1.getPath());
+            FileReader fr1 = new FileReader(a1);
+            BufferedReader br1 = new BufferedReader(fr1);
+            String linea1;
+            Integer er1 = 0;
+            Integer cr1 = 0;
+            Integer ex1 = 0;
+            while ((linea1 = br1.readLine()) != null) {
+                if(linea1.toLowerCase().contains("error")){
+                    er1++;
+                }
+                
+                if(linea1.toLowerCase().contains("crash")){
+                    cr1++;
+                }
+                
+                if(linea1.toLowerCase().contains("exception")){
+                    ex1++;
+                }
+            }
+            fr1.close();
+            br1.close();
+            
             ChartSeries n5xMonkeySerie = new ChartSeries();
             n5xMonkeySerie.setLabel("Nexus 5x");
-            n5xMonkeySerie.set("Errores", 120);
-            n5xMonkeySerie.set("Crashes", 100);
-            n5xMonkeySerie.set("Excepciones", 44);
+            n5xMonkeySerie.set("Errores", er1);
+            n5xMonkeySerie.set("Crashes", cr1);
+            n5xMonkeySerie.set("Excepciones", ex1);
 
             ChartSeries n6xMonkeySerie = new ChartSeries();
             n6xMonkeySerie.setLabel("Nexus 6x");
@@ -65,11 +88,35 @@ public class ReporteControlador implements Serializable {
 
             barModelCalabash = new BarChartModel();
 
+            URL r2 = this.getClass().getResource("/monkey.log");
+            File a2 = new File(r2.getPath());
+            FileReader fr2 = new FileReader(a2);
+            BufferedReader br2 = new BufferedReader(fr2);
+            String linea2;
+            Integer er2 = 0;
+            Integer cr2 = 0;
+            Integer ex2 = 0;
+            while ((linea2 = br2.readLine()) != null) {
+                if(linea2.toLowerCase().contains("error")){
+                    er2++;
+                }
+                
+                if(linea2.toLowerCase().contains("crash")){
+                    cr2++;
+                }
+                
+                if(linea2.toLowerCase().contains("exception")){
+                    ex2++;
+                }
+            }
+            fr2.close();
+            br2.close();
+            
             ChartSeries n5xCalabashSerie = new ChartSeries();
             n5xCalabashSerie.setLabel("Nexus 5x");
-            n5xCalabashSerie.set("Errores", 120);
-            n5xCalabashSerie.set("Crashes", 100);
-            n5xCalabashSerie.set("Excepciones", 44);
+            n5xCalabashSerie.set("Errores", er2);
+            n5xCalabashSerie.set("Crashes", cr2);
+            n5xCalabashSerie.set("Excepciones", ex2);
 
             ChartSeries n6xCalabashSerie = new ChartSeries();
             n6xCalabashSerie.setLabel("Nexus 6x");
@@ -91,18 +138,17 @@ public class ReporteControlador implements Serializable {
             yAxisCalabash.setMin(0);
             yAxisCalabash.setMax(200);
 
-            URL resource = this.getClass().getResource("/ripper.log");
-            File archivo = new File(resource.getPath());
-            FileReader fr = new FileReader(archivo);
-            BufferedReader br = new BufferedReader(fr);
-            String linea;
+            URL r3 = this.getClass().getResource("/ripper.log");
+            File a3 = new File(r3.getPath());
+            FileReader fr3 = new FileReader(a3);
+            BufferedReader br3 = new BufferedReader(fr3);
+            String linea3;
             ripperLog = "";
-            while ((linea = br.readLine()) != null) {
-                ripperLog = ripperLog + linea + "\n";
+            while ((linea3 = br3.readLine()) != null) {
+                ripperLog = ripperLog + linea3 + "\n";
             }
-            fr.close();
-            br.close();
-
+            fr3.close();
+            br3.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
